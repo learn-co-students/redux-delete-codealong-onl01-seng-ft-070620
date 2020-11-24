833dbef0b2d93ca919e719523de5451c07c64110
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-11-23 18:56:16 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import CreateTodo from './components/todos/CreateTodo'
-import TodosContainer from './components/todos/TodosContainer'
+import CreateTodo from './components/todos/CreateTodo';
+import TodosContainer from './components/todos/TodosContainer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <CreateTodo />
         <TodosContainer />
       </div>
